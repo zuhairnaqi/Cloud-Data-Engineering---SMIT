d5b7d0445edde9637136b91a62d8d16b7b0cdbf6
Zuhair Naqi <zuhairnaqi@gmail.com>
2025-12-28 23:43:00 +0500

    feat: Assignment 12 attempt more questions

--- Assignment 12 Quiz ( Cola NEXT analysis)/Assignment 12 Quiz ( Cola NEXT analysis).sql
+++ Assignment 12 Quiz ( Cola NEXT analysis)/Assignment 12 Quiz ( Cola NEXT analysis).sql
@@ -28,16 +28,59 @@ SELECT s.Name, sp.SupplierID, ProductCount FROM (
 -- Q3. Identify products that have been ordered but never returned.
 -- Show ProductID, ProductName, and total order quantity.
 
-with successful_orders_cts AS (
-	SELECT ProductID, count(*) as total FROM PurchaseOrderDetail od
-		WHERE NOT EXISTS (
-			SELECT * FROM Returns r
-			where r.OrderID = od.OrderID
+with returned_produces_cte AS (
+	SELECT DISTINCT ProductID FROM PurchaseOrderDetail od
+		WHERE EXISTS (
+			SELECT 1 FROM ReturnDetail r
+			where r.ProductID = od.ProductID
 		)
-		GROUP BY ProductID
 )
-SELECT p.ProductID, p.Name as ProductName, so.total  FROM successful_orders_cts AS so
+
+SELECT p.ProductID, p.Name as ProductName, pto.TotalOrderQuantity  FROM (
+	SELECT ProductID, count(*) as TotalOrderQuantity FROM SalesOrderDetail od
+		GROUP BY ProductID
+) AS pto
 	INNER JOIN Product p
-	ON p.ProductID = so.ProductID
+	ON p.ProductID = pto.ProductID
+	WHERE NOT EXISTS (
+		SELECT r.ProductID FROM returned_produces_cte r
+		where r.ProductID = p.ProductID
+	)
+
+-- Q4. For each category, find the most expensive product.
+-- Display CategoryID, CategoryName, ProductName, and Price. Use a subquery to get the max price per category.
+
+WITH max_price_product_category AS (
+	SELECT p.ProductID, p.Name, p.CategoryID, p.Price FROM Product p
+		WHERE p.price = (
+			SELECT MAX(price) FROM Product pc
+			WHERE pc.CategoryID = p.CategoryID
+		)
+)
+
+SELECT pc.CategoryID, c.Name as CategoryName, pc.ProductID, pc.Name as ProductName, pc.Price FROM max_price_product_category pc
+	INNER JOIN Category c
+	ON c.CategoryID = pc.CategoryID
+	ORDER BY pc.CategoryID
+
+-- Q5. List all sales orders with customer name, product name, category, and supplier.
+-- For each sales order, display:
+-- OrderID, CustomerName, ProductName, CategoryName, SupplierName, and Quantity.
+
+SELECT 
+	so.OrderID,
+	 c.Name AS CustomerName,
+    p.Name AS ProductName,
+    cat.Name AS CategoryName,
+    m.Name AS ManufacturerName,
+	od.Quantity
+FROM SalesOrder so
+	INNER JOIN SalesOrderDetail od ON od.OrderID = so.OrderID
+	INNER JOIN Customer c ON c.CustomerID = so.CustomerID
+	INNER JOIN Product p ON p.ProductID = od.ProductID
+	INNER JOIN Category cat ON cat.CategoryID= p.CategoryID
+	INNER JOIN manufacturer m ON p.ManufacturerID = m.ManufacturerID
+	ORDER BY so.OrderID, od.ProductID
+
 
 
